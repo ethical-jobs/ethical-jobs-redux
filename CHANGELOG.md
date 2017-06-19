58adbb7fc222867b33aefb4d9449e29e188a786c
andrewmclagan <andrew@ethicaljobs.com.au>
2017-06-19 14:59:39 +1000

    # 0.2.15
    - clearFilters immutble helper
    - clearFilters assertion test helper

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# 0.2.15
+
+- clearFilters immutble helper 
+- clearFilters assertion test helper
--- src/assertions.js
+++ src/assertions.js
@@ -26,10 +26,11 @@ function clearedEntities(reducer, action, initialState) {
 
 /**
  * Asserts a modules updated filter state
- *
- * @author Andrew McLagan <andrew@ethicaljobs.com.au>
+ * @param {object} reducer
+ * @param {function} actionCreator
+ * @param {object} initialState
+ * @return {boolean}
  */
-
 function updatedFilters(reducer, actionCreator, initialState) {
   let state;
   state = reducer(undefined, actionCreator({ foo: 'bar' }));
@@ -37,6 +38,18 @@ function updatedFilters(reducer, actionCreator, initialState) {
   state = reducer(state, actionCreator({ foo: 10000 }));
   const expected = initialState.set('filters', Immutable.fromJS({ bar: 123, foo: 10000 }));
   return Immutable.is(state, expected);
+}
+
+/**
+ * Asserts a modules cleared filter state
+ * @param {object} reducer
+ * @param {function} actionCreator
+ * @param {object} initialState
+ * @return {boolean}
+ */
+function clearFilters(reducer, actionCreator, initialState) {
+  const state = reducer({ foo: 'bar' }, actionCreator());
+  return Immutable.is(state, Immutable.Map());
 }
 
 /**

f756994f192e45639e1d6191e85e9a2833d18dab
Andrew McLagan <andrew@ethicaljobs.com.au>
2018-06-05 11:22:36 +1000

    - Bumping sem ver. - All immutable redux module merge functions should merge state, not replace it. - Removing mergeSearchRequest. State should not be cleared. Can be done manually. - Upgrading package versions.

--- src/__tests__/immutable/mergeCollectionSuccess.spec.js
+++ src/__tests__/immutable/mergeCollectionSuccess.spec.js
@@ -1,9 +1,10 @@
 import { fromJS, is, isOrdered, OrderedMap, List, Set, OrderedSet, Map } from 'immutable';
+import prettyFormat from 'pretty-format';
 import ImmutableUtils from '../../immutable';
 
 describe('mergeCollectionSuccess function', () => {
 
-  const state = fromJS({
+  const beforeState = fromJS({
     fetching: true,
     error: false,
     entities: {
@@ -23,7 +24,7 @@ describe('mergeCollectionSuccess function', () => {
     results: [22,11,55,44,66,77],
   });
 
-  const actual = ImmutableUtils.mergeCollectionSuccess(state, {
+  const afterState = ImmutableUtils.mergeCollectionSuccess(beforeState, {
     data: {
       entities: {
         countries: {
@@ -41,38 +42,77 @@ describe('mergeCollectionSuccess function', () => {
   });
 
   it('returns correct request lifecycle state', () => {
-    expect(actual.get('fetching')).toBe(false);
-    expect(actual.get('error')).toBe(false);
+    expect(afterState.get('fetching')).toBe(false);
+    expect(afterState.get('error')).toBe(false);
   });
 
-  it('it maintains the result order', () => {
-    expect(isOrdered(actual.get('results'))).toBe(true);
+  it('maintains the result order', () => {
+    expect(isOrdered(afterState.get('results'))).toBe(true);
   });
 
   it('returns correct entities state', () => {
-    const expected = fromJS({
+    const shouldBe = fromJS({
       countries: {
-        11: {id: 11, title: 'Australia'},
         22: { id: 22, title: 'Ethiopia' },
         44: { id: 44, title: 'Botswana' },
         55: { id: 55, title: 'Argentina' },
         66: { id: 66, title: 'South Sudan' },
         77: { id: 77, title: 'Chile' },
+        11: { id: 11, title: 'Australia' },
         88: { id: 88, title: 'Somalia' },
         122: { id: 122, title: 'Austria' },
       },
       continents: {
-        12: { id: 12, title: 'Africa' },
-        7: { id: 7, title: 'South America' },
         12: { id: 12, title: 'Europe' },
+        7: { id: 7, title: 'South America' },
       }
     });
-    expect(is(actual.get('entities'), expected)).toBe(true);
+    expect(
+      is(afterState.get('entities'), shouldBe)
+    ).toBe(true)
   });
 
   it('returns correct results state', () => {
-    const expected = OrderedSet([22,11,55,44,66,77,88,122]);
+    const shouldBe = OrderedSet([22,11,55,44,66,77,88,122]);
+    expect(
+      is(afterState.get('results'), shouldBe)
+    ).toBe(true);
+  });
 
-    expect(is(actual.get('results'), expected)).toBe(true);
+  it('can perform complex collection results merging', () => {
+    const secondAfterState = ImmutableUtils.mergeCollectionSuccess(afterState, {
+      data: {
+        entities: {
+          countries: {
+            207: { id: 207, title: 'Taiwan' },
+            88: { id: 88, title: 'Somalia' },
+            209: { id: 209, title: 'Japan' },
+          },
+        },
+        result: [207, 88, 209],
+      },
+    });
+    expect(is(
+      secondAfterState.get('results'),
+      OrderedSet([22, 11, 55, 44, 66, 77, 88, 122, 207, 209])
+    )).toBe(true);
+    const thirdAfterState = ImmutableUtils.mergeCollectionSuccess(secondAfterState, {
+      data: {
+        entities: {
+          countries: {
+            209: { id: 209, title: 'Japan' },
+            88: { id: 88, title: 'Somalia' },
+            217: { id: 217, title: 'Norway' },
+            310: { id: 310, title: 'New Zealand' },
+            301: { id: 301, title: 'Tonga' },
+          },
+        },
+        result: [217, 209, 88, 310, 301],
+      },
+    });
+    expect(is(
+      thirdAfterState.get('results'),
+      OrderedSet([22, 11, 55, 44, 66, 77, 88, 122, 207, 209, 217, 310, 301])
+    )).toBe(true);
   });
 });
--- src/__tests__/immutable/mergeSuccess.spec.js
+++ src/__tests__/immutable/mergeSuccess.spec.js
@@ -3,7 +3,7 @@ import ImmtuableUtils from '../../immutable';
 
 describe('mergeSuccess function', () => {
 
-  const state = fromJS({
+  const beforeState = fromJS({
     fetching: true,
     error: false,
     entities: {
@@ -18,37 +18,52 @@ describe('mergeSuccess function', () => {
       }
     },
     result: 298,
+    results: [22,44,55],
   });
 
-  const actual = ImmtuableUtils.mergeSuccess(state, {
+  const afterState = ImmtuableUtils.mergeSuccess(beforeState, {
     data: {
       entities: {
         countries: {
-          22: { id: 22, title: 'Ethiopia', population: 99390000, size: '1.104m km2', },
+          22: { id: 22, title: 'Ethiopia', population: 99390000, size: '1.104m km2' },
         },
         continents: {
-          12: { id: 12, title: 'Africa', countries: 54,  },
+          12: { id: 12, title: 'Africa', countries: 54 },
         }
       },
       result: 22,
     },
   });
 
-  const expected = fromJS({
-    fetching: false,
-    error: false,
-    entities: {
-      countries: {
-        22: { id: 22, title: 'Ethiopia', population: 99390000, size: '1.104m km2', },
-      },
-      continents: {
-        12: { id: 12, title: 'Africa', countries: 54,  },
-      }
-    },
-    result: 22,
+  it('returns correct request lifecycle state', () => {
+    expect(afterState.get('fetching')).toBe(false);
+    expect(afterState.get('error')).toBe(false);
   });
 
-  it('returns correct success state', () => {
-    expect(is(actual, expected)).toBe(true);
+  it('returns the correct result identifier', () => {
+    expect(
+      is(afterState.get('results'), beforeState.get('results'))
+    ).toBe(true);
+  });
+
+  it('does not effect the results state', () => {
+    expect(afterState.get('result')).toBe(22);
+  });
+
+  it('merges new entities', () => {
+    const shouldBe = fromJS({
+      countries: {
+        22: { id: 22, title: 'Ethiopia', population: 99390000, size: '1.104m km2', },
+        44: { id: 44, title: 'Botswana' },
+        55: { id: 55, title: 'Argentina' },
+      },
+      continents: {
+        12: { id: 12, title: 'Africa', countries: 54 },
+        7: { id: 7, title: 'South America' },
+      }
+    });
+    expect(
+      is(afterState.get('entities'), shouldBe)
+    ).toBe(true);
   });
 });
